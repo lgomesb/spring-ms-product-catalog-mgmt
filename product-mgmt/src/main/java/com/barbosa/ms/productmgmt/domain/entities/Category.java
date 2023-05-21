e9a318614785ec1bbf267bee3c8e661535c141f8
leandro <lgomesb@gmail.com>
2023-05-21 10:43:26 +0100

    Organizing Category Test

--- product-mgmt/src/main/java/com/barbosa/ms/productmgmt/domain/entities/Category.java
+++ product-mgmt/src/main/java/com/barbosa/ms/productmgmt/domain/entities/Category.java
@@ -1,11 +1,12 @@
 package com.barbosa.ms.productmgmt.domain.entities;
 
+import java.util.UUID;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-@Builder()
 @NoArgsConstructor
 @Table(name = "category")
 @Entity
@@ -13,6 +14,13 @@ public class Category extends AbstractEntity {
     
     public Category(String name) {
         super();
+        super.setName(name);
+    }
+    
+    @Builder()
+    public Category(UUID id, String name) {
+        super();
+        super.setId(id);
         super.setName(name);
     }
 }
--- product-mgmt/src/test/java/com/barbosa/ms/productmgmt/services/CategoryServiceTest.java
+++ product-mgmt/src/test/java/com/barbosa/ms/productmgmt/services/CategoryServiceTest.java
@@ -2,10 +2,8 @@ package com.barbosa.ms.productmgmt.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -32,6 +30,13 @@ public class CategoryServiceTest {
 
     @Mock
     private CategoryRepository repository;
+    
+    private Category category;
+    private CategoryRecord categoryRecord;
+    private Given given = new Given();
+    private When when = new When();
+    private Then then = new Then();
+
 
     @BeforeEach
     void setUp() {
@@ -40,65 +45,111 @@ public class CategoryServiceTest {
 
     @Test
     public void shouldSuccessWhenCreate() {
-        String categoryName = "Category-Test-Success";
-        Category category = new Category(categoryName);
-        category.setId(UUID.randomUUID());
-        
-        when(repository.save(any(Category.class)))
-        .thenReturn(category);
-        
-        CategoryRecord record = service.create(new CategoryRecord(null, categoryName));
-        assertNotNull(record);
-        assertNotNull(record.name());
-        assertEquals(record.name(), category.getName());
-        assertNotNull(record.id());
-        assertEquals(record.id(), category.getId());
-
+        given.categoryInicietedForSuccessfulReturn();
+        given.categoryRecordInicietedForSuccessfulReturn();
+        when.saveCategoryEntity();
+        CategoryRecord record = when.callCreateInCategorySerivce();
+        then.shouldBeSuccessfulValidationRules(record);
     }
 
     @Test
     public void shouldSuccessWhenFindById() {
-        String nameCategory = "Category-Test-FindById-Success";
-        Category category = new Category(nameCategory);
-        UUID idCategory = UUID.randomUUID();
-        category.setId(idCategory);
-        when(repository.findById(any(UUID.class))).thenReturn(Optional.of(category));
-        CategoryRecord record = service.findById(idCategory);
-        assertNotNull(record);
-        assertNotNull(record.name());
-        assertEquals(record.name(), category.getName());
-        assertNotNull(record.id());
-        assertEquals(record.id(), category.getId());
+        given.categoryInicietedForSuccessfulReturn();
+        when.findCategoryById();
+        CategoryRecord record = when.callCategoryServiceFindById();
+        then.shouldBeSuccessfulValidationRules(record);
     }
 
     @Test
     public void shouldSuccessWhenUpdate() {
-        String categoryName = "Category-Test-Success";
-        UUID idCategory = UUID.randomUUID();
-        Category category = new Category(categoryName);
-        when(repository.findById(any(UUID.class))).thenReturn(Optional.of(category));
-        when(repository.save(any(Category.class))).thenReturn(category);
-        service.update(new CategoryRecord(idCategory, categoryName));
-        ArgumentCaptor<Category> categoryCaptor = ArgumentCaptor.forClass(Category.class);
-        verify(repository).save(categoryCaptor.capture());
-        assertNotNull(categoryCaptor.getValue());
-        assertEquals(categoryCaptor.getValue().getName(),categoryName);
-        
+        given.categoryInicietedForSuccessfulReturn();
+        given.categoryRecordInicietedForSuccessfulReturn();
+        when.findCategoryById();
+        when.callCategoryServiceFindById();
+        when.saveCategoryEntity();
+        when.callCreateInCategorySerivce();
+        then.shouldBeSuccessfulArgumentValidationByUpdate();        
     }
 
     @Test
     public void delete() {
-        String categoryName = "Category-Test-Success";
-        UUID idCategory = UUID.randomUUID();
-        Category category = new Category(categoryName);
-        when(repository.findById(any(UUID.class))).thenReturn(Optional.of(category));
-        doNothing().when(repository).delete(any(Category.class));
-        service.delete(idCategory);
-        ArgumentCaptor<Category> categoryCaptor = ArgumentCaptor.forClass(Category.class);
-        verify(repository).delete(categoryCaptor.capture());
-        assertNotNull(categoryCaptor.getValue());
-        assertEquals(categoryCaptor.getValue().getName(),categoryName);
-        
+        given.categoryInicietedForSuccessfulReturn();
+        when.findCategoryById();
+        when.deleteCategoryEntity();
+        when.callDelteInCategorySerivce();    
+        then.shouldBeSuccessfulArgumentValidationByDelete();    
+    }
+
+    class Given {
+
+        public UUID creationIdOfCategory() {
+            return UUID.randomUUID();
+        }
+
+        public void categoryInicietedForSuccessfulReturn() {
+           category = Category.builder()
+                        .id(creationIdOfCategory())
+                        .name("Category-Test-Success")
+                        .build();
+        }
+
+        public void categoryRecordInicietedForSuccessfulReturn () {
+            categoryRecord = new CategoryRecord(category.getId(), category.getName());
+        }
+    }
+
+    class When {
+
+        public void saveCategoryEntity() {
+            when(repository.save(any(Category.class)))
+            .thenReturn(category);
+        }
+
+        public void callDelteInCategorySerivce() {
+            service.delete(given.creationIdOfCategory());
+        }
+
+        public void deleteCategoryEntity() {
+            doNothing().when(repository).delete(any(Category.class));
+        }
+
+        public CategoryRecord callCategoryServiceFindById() {
+            return service.findById(given.creationIdOfCategory());
+        }
+
+        public void findCategoryById() {
+            when(repository.findById(any(UUID.class))).thenReturn(Optional.of(category));
+        }
+
+        public CategoryRecord callCreateInCategorySerivce() {
+            return service.create(categoryRecord);
+        }
+
     }
     
+    class Then {
+
+        public void shouldBeSuccessfulValidationRules(CategoryRecord record) {
+            assertNotNull(record);
+            assertNotNull(record.name());
+            assertEquals(record.name(), category.getName());
+            assertNotNull(record.id());
+            assertEquals(record.id(), category.getId());
+        }
+
+        public void shouldBeSuccessfulArgumentValidationByDelete() {
+            ArgumentCaptor<Category> categoryCaptor = ArgumentCaptor.forClass(Category.class);
+            verify(repository).delete(categoryCaptor.capture());
+            assertNotNull(categoryCaptor.getValue());
+            assertEquals(categoryCaptor.getValue().getName(),category.getName());
+        }
+
+        public void shouldBeSuccessfulArgumentValidationByUpdate() {
+            ArgumentCaptor<Category> categoryCaptor = ArgumentCaptor.forClass(Category.class);
+            verify(repository).save(categoryCaptor.capture());
+            assertNotNull(categoryCaptor.getValue());
+            assertEquals(categoryCaptor.getValue().getName(),category.getName());
+        }
+
+    }
 }
