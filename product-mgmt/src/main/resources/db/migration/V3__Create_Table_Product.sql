9a6949c59ff33251739443de054983c10482f25b
leandro <lgomesb@gmail.com>
2023-07-23 20:08:35 +0100

    DB Migration Implementing

---
+++ product-mgmt/src/main/resources/db/migration/V3__Create_Table_Product.sql
@@ -0,0 +1,11 @@
+create table product (
+    id uuid not null,
+    created_by varchar(100) not null default '99999',
+    created_on timestamp(6),
+    modified_by varchar(100),
+    modified_on timestamp(6),
+    name varchar(255) not null not null,
+    status varchar(1) not null default 'A',
+    category_id uuid,
+    primary key (id)
+);
