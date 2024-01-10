e17a2d432b88d6f419197ef1afde8e431606c66b
leandro <lgomesb@gmail.com>
2024-01-10 00:04:27 +0000

    Configure cache with Redis

--- src/main/java/com/barbosa/ms/productmgmt/config/HealthConfig.java
+++ src/main/java/com/barbosa/ms/productmgmt/config/HealthConfig.java
@@ -1,14 +1,20 @@
 package com.barbosa.ms.productmgmt.config;
 
 import com.barbosa.ms.productmgmt.domain.vo.HealthVO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HealthConfig {
 
+    @Value("${spring.data.redis.password}")
+    private String hostCache;
+
     @Bean
     public HealthVO healthVO() {
+        System.out.println("::::::::::::::::: HOST - CACHE :::::::::::: ");
+        System.out.println(hostCache);
         return new HealthVO(Boolean.TRUE);
     }
 }
--- src/main/java/com/barbosa/ms/productmgmt/domain/records/ProductRecord.java
+++ src/main/java/com/barbosa/ms/productmgmt/domain/records/ProductRecord.java
@@ -1,10 +1,11 @@
 package com.barbosa.ms.productmgmt.domain.records;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 import lombok.Builder;
 
 @Builder
-public record ProductRecord(UUID id, String name, UUID idCategory) {
+public record ProductRecord(UUID id, String name, UUID idCategory) implements Serializable {
     
 }
