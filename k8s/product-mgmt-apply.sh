b974b5636b7944f321b17e03699c44f6e952740c
leandro <lgomesb@gmail.com>
2024-01-20 23:42:30 +0000

    Remove k8s and helm

--- k8s/product-mgmt-apply.sh
+++
@@ -1,4 +0,0 @@
-kubectl apply -f secrets/
-kubectl apply -f services/
-kubectl apply -f deployments/
-kubectl apply -f ingress/
--- k8s/product-mgmt-delete.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete -f deployments/
-kubectl delete -f services/
-kubectl delete -f ingress/
-kubectl delete -f secrets/
-kubectl delete -f namespace/
